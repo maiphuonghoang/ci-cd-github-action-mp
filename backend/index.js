3153e2accaedad0b0fa6d8bb7d62b382a7f6d7dd
maiphuonghoang <maiphuonghoangmpk@gmail.com>
2025-02-03 19:30:53 +0700

    trigger be file to run workflow

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ console.log(Sum(102, 106));
 const mongoUri = process.env.mongoUri || 'mongodb://admin:admin@localhost:27017/admin'
 mongoose.connect(mongoUri, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true, 
 }).then(() => {
   console.log('MongoDB connected!');
 }).catch((err) => {
