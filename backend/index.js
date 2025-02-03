9bfcf9b03bd800965dbc172b2ab96a0c800d9070
maiphuonghoang <maiphuonghoangmpk@gmail.com>
2025-02-03 23:55:46 +0700

    add .env

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,6 @@ const articleSchema = new mongoose.Schema({
 });
 const Article = mongoose.model('Article', articleSchema);
 
-
 // Body-parser middleware
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
