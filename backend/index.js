ccf3022d4ac3b47aade749885a9180372d33bc24
maiphuonghoang <maiphuonghoangmpk@gmail.com>
2025-02-04 01:40:36 +0700

    update be-cd-uat

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const Sum = require('./utils');
 const app = express();
 const port = process.env.PORT || 3001;
-
+// uat env 
 console.log(Sum(102, 106));
 const mongoUri = process.env.mongoUri || 'mongodb://admin:admin@localhost:27017/admin'
 mongoose.connect(mongoUri, {
